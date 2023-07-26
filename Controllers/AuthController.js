5bdec060ddfc555636ddcd758ebd31fd56d2906b
keyl1yy <keyly.la@fordeer.io>
2023-07-26 09:45:53 +0700

    udpate user controller

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,8 +1,12 @@
 const UserModel = require('../Models/UserModel');
-
+const bcrypt = require('bcrypt');
 const registerUser = async (req,res) => {
   const {username, password, firstName, lastName} = req.body;
-  const newUser = new UserModel({username, password, firstName, lastName});
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password,salt)
+
+  const newUser = new UserModel({username, hashedPass, firstName, lastName});
   try {
     await newUser.save();
     return res.status(200).json({msg: 'Success', user: newUser})
